ddabcaef1eeaac9c96bafc1546ffdcb8c6e07bcb
enderta <endertanver@gmail.com>
2022-08-25 17:30:47 +0100

    new

--- src/Hero4.jsx
+++ src/Hero4.jsx
@@ -7,7 +7,7 @@ const Hero4 = () => {
         <p>iletişim ve ayrıntılı bilgi için lütfen arayın</p>
         <p>Selahattin Aksoy</p>
         <p> <a href='tel:+905355555555'>+90 552 080 7450</a></p>
-        <img src={require('./images/aboutme.jpeg')} alt='aboutme.jpeg' height={700} width={500}/>
+        <img src={require('./images/aboutme.jpeg')} alt='aboutme.jpeg' height={700} width={400}/>
     </div>
     </div>
   )
